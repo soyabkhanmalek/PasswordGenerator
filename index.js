47df404c8c97f9fb4fb5b13753d4f32d6ba88cc3
soyabkhan <soybmaalek123@gmail.com>
2026-03-16 11:21:10 +0530

    Added the Functionality to choose numbers and symbols while generating Password

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const myButton = document.getElementById("button")
 const slider = document.getElementById("range-slider")
 const sliderLabel = document.getElementById("slider-value")
+const includeNumbers = document.getElementById("includeNumbers")
+const includeSymbols = document.getElementById("includeSymbols")
 
+
+sliderLabel.textContent = slider.value
 let passlength = slider.value
 
 slider.addEventListener("change", function () {
@@ -10,17 +14,34 @@ slider.addEventListener("change", function () {
 })
 const passWord1 = document.getElementById("password1")
 const passWord2 = document.getElementById("password2")
-const charset = [
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "<", ">", "/", "?", "~"]
+
+/*const charset = [
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
     "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "<", ">", "/", "?", "~"
-];
-
+];*/
+let charset = []
 //randompass = Math.floor(Math.random() * charset.length);
 
 function generatepassword(){
     let password = ""
+
+    if (includeNumbers.checked){
+        charset = [...characters,...numbers]
+    } else if (includeSymbols.checked){
+        charset = [...characters,...symbols]
+    } else if (includeSymbols.checked && includeNumbers.checked){
+        charset = [...characters,...numbers,...symbols]
+    } else {
+        charset = [...characters]
+    }
+
+
     for (let i = 0; i < passlength; i++) {
 
         randompass = charset[Math.floor(Math.random() * charset.length)]
